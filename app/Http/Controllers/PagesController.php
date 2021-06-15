d2a8649876d0496b8bb404fd6865aa427615a3f5
wowlegions <support@wowlegions.ru>
2021-06-15 12:23:09 +0300

    Fix Page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Butschster\Head\Facades\Meta;
 use Illuminate\Http\Request;
 use TCG\Voyager\Models\Page;
 
 class PagesController extends Controller
 {
     public function view($id, $slug ) {
-        return view('pages.view', ['page' => Page::where('id', $id)->where('slug', $slug)->firstOrFail()]);
+        $data = Page::where('id', $id)->where('slug', $slug)->firstOrFail();
+        Meta::prependTitle($data->getTranslatedAttribute('title', App()->getLocale(), 'en-gb'))
+            ->setDescription(__('races.races_12'));
+        return view('pages.view', ['page' => $data]);
     }
 }
