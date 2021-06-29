7a4263308fb0a706fcd3957fcf46894483dd339e
Игорь <eurostar33rus@gmail.com>
2021-06-29 21:38:26 +0300

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => '/storage',
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
 
