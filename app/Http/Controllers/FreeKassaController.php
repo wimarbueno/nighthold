0563c23cf7abf0f5278646f1aa2e241fa1c711f1
Игорь <eurostar33rus@gmail.com>
2021-07-04 20:58:30 +0300

    Update FreeKassaController.php

--- app/Http/Controllers/FreeKassaController.php
+++ app/Http/Controllers/FreeKassaController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\HistoryPayment;
-use App\Models\Shadowlands\Account\Account;
+use App\Models\Wotlk\Account\AccountDonate;
 use Illuminate\Http\Request;
 
 class FreeKassaController extends Controller
@@ -13,8 +13,24 @@ class FreeKassaController extends Controller
     {
         $order = HistoryPayment::where('order_id', $request->get('MERCHANT_ORDER_ID'))->first();
         if($order) {
-            $newBalance = Account::balance()->balans + $order->price;
-            Account::setBalance($newBalance);
+
+            $newAccountBalance = AccountDonate::where('id', auth()->user()->accountWotlk->id)->first();
+            if ($newAccountBalance) {
+                AccountDonate::updateOrCreate([
+                    'id' => auth()->user()->accountWotlk->id,
+                ],[
+                    'bonuses' => $newAccountBalance->bonuses  + $order->price,
+                    'total_bonuses' => $newAccountBalance->bonuses  + $order->price
+                ]);
+            } else {
+                AccountDonate::updateOrCreate([
+                    'id' => auth()->user()->accountWotlk->id,
+                ],[
+                    'bonuses' => $order->price,
+                    'total_bonuses' => $order->price
+                ]);
+            }
+
             HistoryPayment::where('order_id', $request->get('MERCHANT_ORDER_ID'))->update(['status' => 1]);
             return redirect('/dashboard/payment/index');
         }
